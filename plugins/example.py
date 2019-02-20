d64277402162ed39788fd3d3a1fbdc04951dd53c
Louis <louis@jurczyk.fr>
2019-02-20 17:33:47 +0200

    Update example.py

--- plugins/example.py
+++ plugins/example.py
@@ -12,17 +12,17 @@ This is an example plugin implementation
 # Thoses variables can be modified in the __init__ function
 #
 
-# @type	{variable name}		| default_value	|Explanation
+# @type	{variable name}		| default_value		|Explanation
 ------------------------------------------------------------------------------------------------
 # @enum {connection_type}	| NONE			| This will determine the object send in the exec() method
-							|				|	- connection_type.RAW: Will give you a raw tcp socket (https://docs.python.org/3/library/socket.html)
-							|				|	- connection_type.WEB: Will give you a Requests object (http://docs.python-requests.org/en/master/)
-# @int  {max_workers}		| 2				| number of threads used during the scanning process
+				|			|	- connection_type.RAW: Will give you a raw tcp socket (https://docs.python.org/3/library/socket.html)
+				|			|	- connection_type.WEB: Will give you a Requests object (http://docs.python-requests.org/en/master/)
+# @int  {max_workers}		| 2			| number of threads used during the scanning process
 # @bool	{verify_ssl}		| False			| verify ssl certificates
-# @str  {ip_range}			| 0.0.0.0/0		| IP range to scan (only accept format with significant bits)
-# @str	{port}				| 80			| Port which will be pinged on IP Addresses
-# @str	{relative_url}		| /				| Requested url for web-based plugins (connection_type.WEB)
-# @int  {timeout}			| 5				| Timeout for web-based plugins (connection_type.WEB)
+# @str  {ip_range}		| 0.0.0.0/0		| IP range to scan (only accept format with significant bits)
+# @str	{port}			| 80			| Port which will be pinged on IP Addresses
+# @str	{relative_url}		| /			| Requested url for web-based plugins (connection_type.WEB)
+# @int  {timeout}		| 5			| Timeout for web-based plugins (connection_type.WEB)
 
 """
 import core.logger as log
