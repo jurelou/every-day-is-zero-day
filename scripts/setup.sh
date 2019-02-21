04a01e2ade8a52608b53fd36637ee58f2013e775
fumery <henry.fumery@epitech.eu>
2019-02-21 19:11:11 +0200

    delete it

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
-cd ../masscan && make -j4
-if [ $? -eq 2 ]; then
-    cd .. ; rm -rf masscan
-    git clone https://github.com/robertdavidgraham/masscan.git ; cd masscan
-    make -j4
-fi
-
-cd ..
+cd masscan && make -j4
 virtualenv venv
 source venv/bin/activate
 pip install -r requirements.txt
