8ae443ca14e44e045323d811c7fcb6e68d8e7b46
fumery <henry.fumery@epitech.eu>
2019-02-21 18:43:54 +0200

    add clone masscan

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-cd masscan && make -j4
+cd ../masscan && make -j4
+if [ $? -eq 2 ]; then
+    cd .. ; rm -rf masscan
+    git clone https://github.com/robertdavidgraham/masscan.git ; cd masscan
+    make -j4
+fi
+
 cd ..
 virtualenv venv
 source venv/bin/activate
